4fd937d00e201f58d0158d2be15749685403f83b
Correia Lopes <jlopes@fe.up.pt>
2021-01-20 18:23:51 +0000

    added lectures/24/rock_scissors.py

--- lectures/22/spiral.py
+++ lectures/22/spiral.py
@@ -6,10 +6,11 @@ Created on Fri Dec  6 17:34:01 2019
 @author: jlopes
 """
 
+
 # Don't do this at home!
-def h2(t, size):
+def h2(t):
     """ Draw the next step of a spiral on each call. """
-    # global size      # FIXME
+    global size      # FIXME
     t.stamp()        # Leave an impression on the canva
     size += 3        # Increase the size on every iteration
     t.right(24)      # Move tess along
@@ -19,8 +20,9 @@ def h2(t, size):
 import turtle
 
 # have a turtle
-ww = turtle.Screen()
-ww.bgcolor("lightgreen")
+wn = turtle.Screen()
+wn.setup(800, 680)
+wn.bgcolor("lightgreen")
 tess = turtle.Turtle()
 tess.shape("turtle")
 tess.color("blue")
@@ -31,10 +33,20 @@ size = 20            # Initial size
 
 # draw it!
 for _ in range(30):
-    # h2(tess)         # Draw the next step
-    h2(tess, size)
-    size += 3        # Increase the size on every iteration
+    h2(tess)         # Draw the next step
+    size += 1        # Increase the size on every iteration
 
 # close down
-ww.mainloop()
+wn.mainloop()
 turtle.bye()
+
+
+#def h2(t, size):
+#    """ Draw the next step of a spiral on each call. """
+#    # global size      # FIXME
+#    t.stamp()        # Leave an impression on the canva
+#    size += 3        # Increase the size on every iteration
+#    t.right(24)      # Move tess along
+#    t.forward(size)  # ... and turn her
+#
+#    h2(tess, size)
