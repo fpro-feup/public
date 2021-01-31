17f15daf8622a0409abc6998e44d1870c4e55c40
Correia Lopes <jlopes@fe.up.pt>
2021-01-31 22:33:41 +0000

    added find_my_room.py

---
+++ find_my_room.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+student_dist = { 
+  'B101': ("Adam Gershenson Nogueira", "Alexandre Guimaraes Gomes Correia"),
+  'B104': ("Alexandre Manuel Luz Rodrigues da Costa", "Duarte Filipe Campos Barbosa Lopes"),
+  'B105': ("Eduardo Duarte da Silva", "Francisco Lopes Mendonça"),
+  'B201': ("Francisco Miguel Alcobia Maia Prada", "Guilherme Soares Sequeira"),
+  'B203': ("Guilherme Valler Moreira", "Igor Liberato de Castro"),
+  'B204': ("Igor Rodrigues Diniz", "Joana Inês Gonçalves dos Santos"),
+  'B207': ("Joana Vale Amaro de Sousa", "José Marcelino Zacarias Júnior"),
+  'B208': ("José Maria Borges Pires do Couto e Castro", "Mariana Mota Azevedo"),
+  'B213': ("Mariana Solange Monteiro Rocha", "Pedro Miguel Magalhães Nunes"),
+  'B304': ("Pedro Miguel Moreira Ramalho", "Ricardo Filipe Resende Teixeira Pereira"),
+  'B305': ("Ricardo Jorge Costa Nogueira", "Sérgio Manuel de Sousa Carvalho e Boura Carvalhais"),
+  'B311': ("Simião Jorge Mavie", "Walter Muhanyele Massango") }
+
+from unidecode import unidecode
+
+def name_in(name, left, right):
+    return unidecode(left).lower() <= unidecode(name).lower() <= unidecode(right).lower()
+
+name = input("Give me the full Name? ")
+room = [k for k, v in student_dist.items() if name_in(name, v[0], v[1])]
+if len(room): print(f"In the next assign '{name}' goes to '{room[0]}'!")
